aa61014333dc0fef6f11899d08b9700e6655ef47
BaoTheParo <tanthaibao123@gmail.com>
2025-03-25 08:31:06 +0700

    merge

--- backend/spotify_clone/settings.py
+++ backend/spotify_clone/settings.py
@@ -39,9 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'music',
     'rest_framework',
+    'rest_framework_simplejwt',  # Thêm dòng này
     'corsheaders',
 ]
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import React, { useContext } from 'react';
+import React, { Component, useContext } from 'react';
 import Sidebar from './components/sidebar';
 import Player from './components/Player';
 import Display from './components/Display';
 import { PlayerContext } from './context/PlayerContext';
-
+import Messages from './components/Messages';
 const App = () => {
 
   const { audioRef, track } = useContext(PlayerContext)
-
   return (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
         <Display />
       </div>
+      <PrivateRoute path="/messages" component={Messages} exact/>
       <Player />
       <audio ref={audioRef} src={track.file} preload='auto'></audio>
     </div>
