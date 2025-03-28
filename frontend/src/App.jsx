f7142c1e2c8f771a304d410f21e72bad3881bc15
Broccoli <pvt8626@gmail.com>
2025-03-28 21:26:35 +0700

    update login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,15 @@ import Sidebar from './components/sidebar';
 import Player from './components/Player';
 import Display from './components/Display';
 import { PlayerContext } from './context/PlayerContext';
+import Login from './components/LogIn';
+import SignUp from './components/SignUp';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import "./App.css";
 const App = () => {
 
   const { audioRef, track } = useContext(PlayerContext)
-  return (
+
+  const MainLayout = () => (
     <div className="h-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
@@ -15,6 +20,25 @@ const App = () => {
       <Player />
       <audio ref={audioRef} preload='auto'></audio>
     </div>
+  );
+
+  const LoginLayout = () => (
+    <div className="app h-screen bg-black">
+      <Login />
+    </div>
+  );
+
+  const SignupLayout = () => (
+    <div className="app h-screen bg-black">
+      <SignUp />
+    </div>
+  );
+  return (
+    <Routes>
+      <Route path="/login" element={<LoginLayout />} />
+      <Route path="/signup" element={<SignupLayout />} />
+      <Route path="/*" element={<MainLayout />} />
+    </Routes>
   )
 }
 
